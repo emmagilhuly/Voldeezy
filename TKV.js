ca34d206753071764bf1f23498f7c186022f3a01
Emma Gilhuly <emmagilhuly@me.com>
2015-10-14 17:18:36 -0700

    2 player

--- TKV.js
+++ TKV.js
@@ -1,9 +1,14 @@
-var game = {score: 0, ellapsedTime: 60};
+var playerOne = {score: 0}
+var playerTwo = {score: 0}
+var game = {
+    ellapsedTime: 5,
+    currentPlayer: playerOne
+};
 var $timer = $('.timer');
 var $picture = $(".picture")
 var $displayQuote = $("#displayquote")
 var $scoreboard = $('.score')
-var $restart = $("button")
+var $switchPlayer = $("button")
 
 //set donald as an object with 0-9 quotes and picture
 var donald = {
@@ -25,16 +30,16 @@ $picture: $("#donald")
 //set kanye as an object with 10-19 quotes and picture
 var kanye = {
 quote: [
-  "I hate politics. I’m not a politician at all.",
-  "I am so credible and so influential and so relevant that I will change things.",
-  "I will be the leader of a company that ends up being worth billions of dollars because I got the answers.",
-  "There’s certain things where I’m like, am I supposed to say this in the press? Am I not supposed to say this?",
-  "I feel like I'm too busy writing history to read it.",
-  "Nothing in life is promised except death.",
-  "I still think I am the greatest.",
-  "I live and breathe every element in life.",
-  "The media crucify me like they did Christ.",
-  "When you're the absolute best, you get hated on the most.",
+    "I hate politics. I’m not a politician at all.",
+    "I am so credible and so influential and so relevant that I will change things.",
+    "I will be the leader of a company that ends up being worth billions of dollars because I got the answers.",
+    "There’s certain things where I’m like, am I supposed to say this in the press? Am I not supposed to say this?",
+    "I feel like I'm too busy writing history to read it.",
+    "Nothing in life is promised except death.",
+    "I still think I am the greatest.",
+    "I live and breathe every element in life.",
+    "The media crucify me like they did Christ.",
+    "When you're the absolute best, you get hated on the most.",
 ],
 $picture: $("#kanye")
 }
@@ -63,98 +68,94 @@ allQuotes.push.apply(allQuotes, kanye.quote);
 allQuotes.push.apply(allQuotes, voldemort.quote);
 
 var randnums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+// var randnums = [ ];
+// for (i= 0; i < 30; i++){
+//     randnums.push()
+// }
 
 function getRandomQuote(){
-  quoteIndex = Math.floor(Math.random() * randnums.length);
-  var randomNumber = randnums.splice(quoteIndex, 1);
-  console.log('randomNumber', randomNumber);
+
+  var randomPosition = Math.floor(Math.random() * randnums.length); //get random position based on length of randnums
+  quoteIndex = randnums.splice(randomPosition, 1)[0]; //get quoteIndex using random position inside randnums, taking out used number
+  //console.log('randnums.length', randnums.length);
+  console.log('quoteIndex', quoteIndex);
   if (randnums.length === 0) {
-    allQuestionsAnswered();
-    }
-  return allQuotes[randomNumber];
+    clearInterval(displayTimer);
+    disableEventHandlers();
+    alert ("All quotes used!")
+    return 'All quotes used';
+    allQuestionsAsked()
+    game = {ellapsedTime: 0};
+  } else {
+    return allQuotes[quoteIndex];
+  }
 }
 
-
 $displayQuote.text(getRandomQuote());
-
 
 // enable click on pictures
 function setEventHandlers(){
   $(donald.$picture).click (function (){
   if (quoteIndex <= 9) {
-    // alert ("You picked Trump, you are correct!")
     $("#donaldhalo").css("visibility","visible");
     setTimeout(function (){
         $("#donaldhalo").css("visibility","hidden");
     }, 1000);
-    game.score++;
-    $scoreboard.text("Score: "+ game.score);
+    game.currentPlayer.score++;
+    $scoreboard.text("Score: "+ game.currentPlayer.score);
     $scoreboard.fadeIn(750).fadeOut(750).fadeIn(750)
-    //console.log(getRandomQuote());
     $displayQuote.text(getRandomQuote())
   } else if (quoteIndex >= 10) {
-    // alert ("You choose the wrong answer")
-    // $(this).fadeIn(750).fadeOut(750).fadeIn(750)
-    $("#donaldevil").css("visibility","visible");
-    setTimeout(function (){
-        $("#donaldevil").css("visibility","hidden");
-    }, 1000);
-    game.score-=1;
-    $displayQuote.text(getRandomQuote());
-    $scoreboard.text("Score: "+ game.score);
+      $("#donalddevil").css("visibility","visible");
+      setTimeout(function (){
+          $("#donalddevil").css("visibility","hidden");
+      }, 1000);
+    game.currentPlayer.score-=1;
+    $scoreboard.text("Score: "+ game.currentPlayer.score);
     $scoreboard.fadeIn(750).fadeOut(750).fadeIn(750)
+    $displayQuote.text(getRandomQuote());
   }
   });
 
   $(kanye.$picture).click(function (){
   if (quoteIndex >= 10 && quoteIndex <= 19 ) {
-    // alert ("You picked Kanye, you are correct!")
     $("#kanyehalo").css("visibility","visible");
     setTimeout(function (){
         $("#kanyehalo").css("visibility","hidden");
     }, 1000);
-    // $(this).toggleClass('clickedcorrect');
-    // setTimeout(function (){
-    //     $(kanye.$picture).toggleClass('clickedcorrect');
-    // }, 1000);
-    game.score++;
-    $scoreboard.text("Score: " + game.score);
+    game.currentPlayer.score++;
+    $scoreboard.text("Score: " + game.currentPlayer.score);
     $scoreboard.fadeIn(750).fadeOut(750).fadeIn(750)
-    //console.log(getRandomQuote());
     $displayQuote.text(getRandomQuote())
   } else if (quoteIndex >= 20 || quoteIndex <=9) {
-    // alert ("You choose the wrong answer")
-    // $(this).fadeIn(750).fadeOut(750).fadeIn(750)
     $("#kanyedevil").css("visibility","visible");
     setTimeout(function (){
         $("#kanyedevil").css("visibility","hidden");
     }, 1000);
-    game.score-=1;
-    $displayQuote.text(getRandomQuote());
-    $scoreboard.text("Score: "+ game.score);
+    game.currentPlayer.score-=1;
+    $scoreboard.text("Score: "+ game.currentPlayer.score);
     $scoreboard.fadeIn(750).fadeOut(750).fadeIn(750)
+    $displayQuote.text(getRandomQuote());
   }
   })
 
   $(voldemort.$picture).click(function (){
   if (quoteIndex >= 20 ) {
-    // alert ("You picked Voldemort, you are correct!")
     $("#voldemorthalo").css("visibility","visible");
     setTimeout(function (){
         $("#voldemorthalo").css("visibility","hidden");
     }, 1000);
-    game.score++;
-    $scoreboard.text("Score: "+ game.score);
-    //console.log(getRandomQuote());
+    game.currentPlayer.score++;
+    $scoreboard.text("Score: "+ game.currentPlayer.score);
     $displayQuote.text(getRandomQuote())
   } else if (quoteIndex < 20) {
-    // alert ("You choose the wrong answer")
     $("#voldemortdevil").css("visibility","visible");
     setTimeout(function (){
         $("#voldemortdevil").css("visibility","hidden");
-    }, 1000);    game.score-=1;
+    }, 1000);
+    game.currentPlayer.score-=1;
+    $scoreboard.text("Score: "+ game.currentPlayer.score);
     $displayQuote.text(getRandomQuote());
-    $scoreboard.text("Score: "+ game.score);
   }
   })
 }
@@ -167,7 +168,7 @@ function disableEventHandlers(){
 }
 
 //click restart to reset
-$restart.click(function(){
+$switchPlayer.click(function(){
   reset();
 })
 
@@ -176,12 +177,33 @@ var displayTimer;
 
 //reset time and score and start timer
 function reset(){
-  game = {score: 0, ellapsedTime: 60};
+  game = {ellapsedTime: 5, currentPlayer: playerTwo};
   $timer.text("Time: 60");
   $scoreboard.text("Score: 0");
   clearInterval(displayTimer);
   startTimer();
 }
+
+function getWinner(){
+    $("button").hide()
+    $timer.text("Player One Score: " + playerOne.score + ",  Player Two Score: " + playerTwo.score)
+    if (playerOne.score > playerTwo.score){
+         $scoreboard.text("Player One has won!");
+    } else if (playerTwo.score > playerOne.score){
+         $scoreboard.text("Player Two has won!");
+    } else if (playerOne.score === playerTwo.score){
+        $scoreboard.text("Tie game!");
+    }
+}
+
+// function allQuestionsAsked(){
+//     if (game.currentPlayer === playerOne) {
+//         $timer.text("Press Switch Player for Player Two");
+//     } else if (game.currentPlayer === playerTwo){
+//         $timer.text("Game over");
+//         getWinner();
+//     }
+// }
 
 //disableEvent Handlers, alert time is up and press refresh to start again
 function gameOver(){
@@ -189,7 +211,12 @@ function gameOver(){
   disableEventHandlers();
   alert ("Time is up!");
   $timer.text("Time is up");
-  $scoreboard.text("Press restart to play again");
+  if (game.currentPlayer === playerOne) {
+      $scoreboard.text("Press Switch Player for Player Two");
+  } else if (game.currentPlayer === playerTwo){
+      $scoreboard.text("Game over");
+      getWinner();
+  }
 }
 
 //Display time and when time is finished game over
